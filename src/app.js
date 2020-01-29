ed02fbaee1fe13f8c05f0f00bc8c33185525c80a
Albert-dias <albert.uft@gmail.com>
2020-01-29 20:46:01 -0300

    config database app

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const routes = require('./routes')
+import express from 'express';
+import routes from './routes';
 
-class App{
-    constructor(){
-        this.server = express();
+class App {
+  constructor() {
+    this.server = express();
 
-        this.middlewares();
-        this.routes();
-    }
+    this.middlewares();
+    this.routes();
+  }
 
-    middlewares(){
-        this.server.use(express.json());
-    }
-    routes(){
-        this.server.use(routes);
-    }
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
 }
 
-module.exports = new App().server;
+export default new App().server;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: "Hello World" });
-});
+routes.get('/', (req, res) => res.json({ message: 'Hello Albert' }));
 
-module.exports = routes;
+export default routes;

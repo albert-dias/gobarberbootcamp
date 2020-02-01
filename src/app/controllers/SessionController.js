b57a5e2088a75a256671bbc31b40f359fe80c2b7
Albert-dias <albert.uft@gmail.com>
2020-02-01 17:31:37 -0300

    End module 2.1

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -12,6 +12,11 @@ class SessionController {
         .required(),
       password: Yup.string().required(),
     });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });

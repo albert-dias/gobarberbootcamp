8aa0a7bf2b6fc919bd5302f018afb037155b1ace
Albert-dias <albert.uft@gmail.com>
2020-02-05 22:07:26 -0300

    create notifications

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notifications from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -83,6 +85,22 @@ class AppointmentController {
       date: hourStart,
     });
 
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      {
+        locale: pt,
+      }
+    );
+
+    await Notifications.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id,
+    });
     return res.json(appointment);
   }
 }
